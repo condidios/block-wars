3febe5b8c0ade3bc9bb0868bc02a7284d6e7caea
Murat Bastug <muratcanbastug1@gmail.com>
2024-02-13 23:22:06 +0300

    Clone Ball Skill

--- Assets/Scripts/BallInıt.cs
+++ Assets/Scripts/BallInıt.cs
@@ -7,7 +7,9 @@ using Random = UnityEngine.Random;
 public class BallInıt : MonoBehaviour
 {
     public float speed = 500f;
-
+    public bool isCloned = false;
+    public int cloneCount = 2;
+    public float cloneLife = 3f;
     private Rigidbody2D rb { get; set; }
 
     // Start is called before the first frame update
@@ -17,10 +19,14 @@ public class BallInıt : MonoBehaviour
     }
     private void Start()
     {
-        Invoke(nameof(SetRandomTrajectory),1f);
+        Invoke(nameof(SetRandomTrajectory), 1f);
+        if (isCloned)
+        {
+            Invoke(nameof(CloneBalls), 1f);
+        }
     }
 
-    
+
 
     private void SetRandomTrajectory()
     {
@@ -35,5 +41,19 @@ public class BallInıt : MonoBehaviour
             force.y = 1;
         }
         rb.AddForce(force.normalized * speed);
+    }
+
+    private void CloneBalls()
+    {
+        Vector2 initialDirection = Quaternion.Euler(0, 0, 30f) * Vector2.up;
+        for (int i = 0; i < cloneCount; i++)
+        {
+            Vector2 force = Quaternion.Euler(0, 0, -30f * i) * initialDirection;
+            GameObject clone = Instantiate(gameObject, transform.position, Quaternion.identity);
+            clone.GetComponent<BallInıt>().isCloned = false;
+            Rigidbody2D cloneRb = clone.GetComponent<Rigidbody2D>();
+            cloneRb.AddForce(force.normalized * speed);
+            Destroy(clone, cloneLife);
+        }
     }
 }
